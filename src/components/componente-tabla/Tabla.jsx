d32c880f9fb7a4c7fc92e81e9309d0e3889469d4
Jean Trujillo <00zacks00@gmail.com>
2024-05-15 21:00:56 -0500

    Pedidos ordenados del dia mas reciente

--- src/components/componente-tabla/Tabla.jsx
+++ src/components/componente-tabla/Tabla.jsx
@@ -6,6 +6,8 @@ import Pedidos from './Filas/Pedidos'
 
 export default function Tabla(props) {
 
+    console.log(props)
+
     return (
         <div className="relative h-[800px] overflow-x-auto">
             <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
@@ -28,14 +30,23 @@ export default function Tabla(props) {
                 )):null
                 }
 
-                {props.fila == "pedidos" ? props.data.map((datos) => (
-                        <FilaPedidos
-                            key={datos._id}
-                            {...datos}
-                            actualizar_tabla={props.actualizar_tabla} // fetchData como parametro para poder actualizar la tabla
-                        />
-                    )):null
+                {props.fila === "pedidos" ? 
+                    props.data
+                        .sort((a, b) => {
+                            const fechaA = a.fecha.split('/').reverse().join('-'); // Convertir la fecha a formato "yyyy-mm-dd" para que sea interpretable por Date
+                            const fechaB = b.fecha.split('/').reverse().join('-');
+                            return new Date(fechaB) - new Date(fechaA);
+                        }) // Ordenar por fecha
+                        .map((datos) => (
+                            <FilaPedidos
+                                key={datos._id}
+                                {...datos}
+                                actualizar_tabla={props.actualizar_tabla} // fetchData como parametro para poder actualizar la tabla
+                            />
+                        ))
+                    : null
                 }
+
                 </tbody>
             </table>
         </div>
